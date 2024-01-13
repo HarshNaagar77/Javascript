ff107a15d37bc0538e0ecdc49da19463b0ded903
HarshNaagar77 <naagarharsh70@gmail.com>
2024-01-13 21:06:00 +0530

    changes

---
+++ Concepts/1_(let_const_var).js
@@ -0,0 +1,15 @@
+const number = 9311857195
+var name = "HARSH"
+let surname = "NAAGAR"
+
+name = "harsh"
+surname = "naagar"
+console.log(name)                           // Displays one variable at a time
+console.log(number,name,surname)            // Displays multiple variables at a time
+
+
+
+// Const -> Can not be overwrite 
+// Var -> can be overwrite but if there are more than one varibale of same name it overwrites all
+// Let -> 1. Can be overwrite but do not overwrite all
+//        2. In modern js we use let to make code more efficient
