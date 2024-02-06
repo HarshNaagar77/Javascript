3b336e9421971b3f66fe322c048d5a958624b1d2
HarshNaagar77 <naagarharsh70@gmail.com>
2024-02-06 22:48:38 +0530

    javascript

--- projects/Weather App/weather.js
+++ projects/Weather App/weather.js
@@ -1,23 +1,26 @@
 
+const load = document.querySelector(".load")
 const form = document.querySelector('form')
 form.addEventListener('submit' , function(e){
     e.preventDefault()
+    load.style.display = "block"
     const cityName = document.querySelector("#city").value
     const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${cityName}&days=3`;
     const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '25c27d3856mshb1d6ade997a13ebp1e8d2cjsnce8b2004be5d',
-		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-	}
-};
-
-fetch(url ,options)
-.then((response) => {
-    return response.json()
-})
-.then( (data) => {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '25c27d3856mshb1d6ade997a13ebp1e8d2cjsnce8b2004be5d',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+    fetch(url ,options)
+    .then((response) => {
+        return response.json()
+    })
+    .then( (data) => {
     console.log(data)
+    setTimeout(() => {
+ 
     document.querySelector("#temphead").innerHTML = `${data.current.temp_c}°C` 
     // document.querySelector("#image").src = `${data.current.condition.icon}` 
     document.querySelector("#date").innerHTML = `${data.current.last_updated}` 
@@ -51,9 +54,72 @@ fetch(url ,options)
     document.querySelector(".sunset").innerHTML = `Sunset ${data.forecast.forecastday[0].astro.sunset}`
     document.querySelector(".moonset").innerHTML = `Moonset ${data.forecast.forecastday[0].astro.moonset}`
     document.querySelector(".phase").innerHTML = `${data.forecast.forecastday[0].astro.moon_phase}`
+    load.style.display = "none"
+}, 500);
 })
 .catch( (error) => {
     console.log(error)
 })
+       
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+    const body = document.body;
+    const flex = document.querySelector(".flex2")
+    const btntext = document.querySelector(".btntext")
+    const todColor = document.querySelector(".today2")
+    const tomColor = document.querySelector(".tomorrow")
+    let isDarkTheme = false;
+    const darkbox = document.querySelectorAll(".box")
+
+    themeToggleBtn.addEventListener('click', function () {
+        isDarkTheme = !isDarkTheme;
+
+        if (isDarkTheme) {
+            // Switch to dark theme
+            body.classList.add('dark-theme');
+            flex.classList.add("dark-theme");
+            btntext.innerHTML = "Light Mode"
+            darkbox.forEach( (e) => {
+                e.classList.add("dark-theme")
+            })
+            todColor.classList.add("dark-theme")
+            tomColor.classList.add("dark-theme")
+        } else {
+            // Switch to light theme
+            body.classList.remove('dark-theme');
+            flex.classList.remove("dark-theme")
+            btntext.innerHTML = "Dark Mode"
+            darkbox.forEach( (e) => {
+                e.classList.remove("dark-theme")
+            })
+            todColor.classList.remove("dark-theme")
+            tomColor.classList.remove("dark-theme")
+        }
+
+        // function date(){
+        //     }
+        // date()
+    });
+});
+
+    document.querySelector(".tomorrow").onclick = function(){
+        document.querySelector(".today2").style.borderBottom = "none"
+        document.querySelector(".tomorrow").style.borderBottom = "1px solid"
+        document.querySelector(".tomorrow").style.opacity = "1"
+        document.querySelector(".today2").style.opacity = "0.7"
+        
+    }
+    
+    document.querySelector(".today2").onclick = function(){
+        document.querySelector(".tomorrow").style.borderBottom = "none"
+        document.querySelector(".today2").style.borderBottom = "1px solid"
+        document.querySelector(".today2").style.opacity = "1"
+        document.querySelector(".tomorrow").style.opacity = "0.7"
+    }
+
+
+
+
 
